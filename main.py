5baf2b8aa81138299eaba10c71f04874c19e2071
Antonello Contini <antonello.contini@outlook.it>
2020-12-29 17:16:41 +0100

    more generic plot method

--- main.py
+++ main.py
@@ -51,14 +51,14 @@ def read_ARPAV_station(data_filename):
 # Si passano due dataframe e l'elenco di colonne in comune
 # i due dataframe devono avere la stessa frequenza temporale
 # vedi df.resample()
-def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, window=24, save_graphs=True, show=True):
+def similarity_common_variables(reference_df, test_df, reference_name, test_name, variables=None, window=24, save_graphs=True, show=True):
     def pearson(ser):
         try:
-            arpav_ser = arpav_df[v][ser.index].dropna()
-            ibe_ser = ibe_df[v][ser.index].dropna()
-            if arpav_ser.nunique() <= 1 or ibe_ser.nunique() <= 1:
+            reference_ser = reference_df[v][ser.index].dropna()
+            test_ser = test_df[v][ser.index].dropna()
+            if reference_ser.nunique() <= 1 or test_ser.nunique() <= 1:
                 return np.nan
-            r, p = stats.pearsonr(arpav_ser, ibe_ser)
+            r, p = stats.pearsonr(reference_ser, test_ser)
             return r
         except (KeyError, ValueError) as e:
             return np.nan
@@ -67,19 +67,19 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
         try:
             # RMSE (squared=False)
             # sklearn calcola MSE = sum_i^n((arpav[i] - ibe[i])**2)/n
-            r = mean_squared_error(arpav_df[v][ser.index].dropna(),
-                                                                ibe_df[v][ser.index].dropna(), squared=False)
+            r = mean_squared_error(reference_df[v][ser.index].dropna(),
+                                   test_df[v][ser.index].dropna(), squared=False)
             return r
         except (KeyError, ValueError) as e:
             return np.nan
 
     def nrmse(ser):
-        min_value = arpav_df[v].min()
-        max_value = arpav_df[v].max()
+        min_value = reference_df[v].min()
+        max_value = reference_df[v].max()
         try:
             # RMSE (squared=False)
-            r = mean_squared_error(arpav_df[v][ser.index].dropna(),
-                                                                ibe_df[v][ser.index].dropna(), squared=False)
+            r = mean_squared_error(reference_df[v][ser.index].dropna(),
+                                   test_df[v][ser.index].dropna(), squared=False)
             nr = r * 100 / (max_value - min_value)
             return nr
         except (KeyError, ValueError) as e:
@@ -99,17 +99,19 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
     pearson_rs = {}
     nrmses = {}
     for v in variables:
-        if v in arpav_df and v in ibe_df:
-            unique_df = pd.concat([arpav_df[v].rename(f"ARPAV {v}"), ibe_df[v].rename(f"IBE {v}")], axis=1).dropna()
+        if v in reference_df and v in test_df:
+            ref_v_name = f"REFERENCE {v}"
+            test_v_name = f"TEST {v}"
+            unique_df = pd.concat([reference_df[v].rename(ref_v_name), test_df[v].rename(test_v_name)], axis=1).dropna()
             f, ax = plt.subplots(nrows=3, sharex=True)
             pearson_ax = ax[0]
             rmse_ax = ax[1]
             data_ax = ax[2]
             print(v)
-            rol = arpav_df[v].rolling(window=window)
+            rol = reference_df[v].rolling(window=window)
 
             rol.apply(pearson, raw=False).plot(ax=pearson_ax)
-            overall_r, overall_p = stats.pearsonr(unique_df[f"ARPAV {v}"], unique_df[f"IBE {v}"])
+            overall_r, overall_p = stats.pearsonr(unique_df[ref_v_name], unique_df[test_v_name])
             overall_r = overall_r
             pearson_rs[v] = overall_r
             pearson_ax.set_ylim([-1, 1])
@@ -118,9 +120,9 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
 
             rmse_ser = rol.apply(rmse, raw=False)
             # rmse_avg = rmse_ser.mean()
-            overall_rmse = mean_squared_error(unique_df[f"ARPAV {v}"], unique_df[f"IBE {v}"], squared=False)
-            if unique_df[f"ARPAV {v}"].max() - unique_df[f"ARPAV {v}"].min() > 0:
-                nrmses[v] = overall_rmse / (unique_df[f"ARPAV {v}"].max() - unique_df[f"ARPAV {v}"].min())
+            overall_rmse = mean_squared_error(unique_df[ref_v_name], unique_df[test_v_name], squared=False)
+            if unique_df[ref_v_name].max() - unique_df[ref_v_name].min() > 0:
+                nrmses[v] = overall_rmse / (unique_df[ref_v_name].max() - unique_df[ref_v_name].min())
             rmse_ser.plot(ax=rmse_ax)
             rmse_ax.axhline(overall_rmse, color="red", linestyle="--")
             rmse_ax.set_title(f"{v} - Moving window RMSE - RMSE complessivo: {overall_rmse:.3f}")
@@ -134,15 +136,15 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
             # ax[2].set_title(f"Mov. window Normalized RMSE - {v} Average NRMSE: {nrmse_avg:.3f}%")
             # ax[2].set_ylabel("%")
 
-            arpav_df[v].plot(ax=data_ax, label=f"ARPAV {v}")
-            ibe_df[v].plot(ax=data_ax, label=f"IBE {v}")
-            data_ax.set_title(f"Confronto tra ARPAV e IBE {ibe_name}")
+            reference_df[v].plot(ax=data_ax, label=f"{reference_name} {v}")
+            test_df[v].plot(ax=data_ax, label=f"{test_name} {v}")
+            data_ax.set_title(f"Confronto tra ARPAV e {test_name}")
             if v in units:
                 data_ax.set_ylabel(units[v])
             data_ax.legend()
             plt.tight_layout()
             if save_graphs:
-                graph_directory = Path(f"similarity_graphs/{ibe_name}")
+                graph_directory = Path(f"similarity_graphs/{test_name}")
                 graph_directory.mkdir(parents=True, exist_ok=True)
                 graph_filename = graph_directory.joinpath(f"{v}.png")
                 plt.savefig(graph_filename)
@@ -152,7 +154,7 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
         pearson_df = pd.DataFrame.from_dict(pearson_rs, orient="index", columns=["Pearson"])
         pearson_df.plot.bar(ax=summary_ax)
         summary_ax.set_ylim([-1, 1])
-        summary_ax.set_title(f"Correlazione tra {ibe_name} e ARPAV")
+        summary_ax.set_title(f"Correlazione tra {test_name} e {reference_name}")
         for p in summary_ax.patches:
             h = p.get_height()
             if h > 0:
@@ -161,7 +163,7 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
                 label_y = 0.05
             summary_ax.annotate(f"{p.get_height():.3f}", (p.get_x()+p.get_width()/2., label_y), ha="center")
         if save_graphs:
-            graph_directory = Path(f"similarity_graphs/{ibe_name}")
+            graph_directory = Path(f"similarity_graphs/{test_name}")
             graph_directory.mkdir(parents=True, exist_ok=True)
             graph_filename = graph_directory.joinpath(f"summary.png")
             plt.savefig(graph_filename)
@@ -171,13 +173,13 @@ def similarity_common_variables(arpav_df, ibe_df, ibe_name, variables=None, wind
         nrmse_df = pd.DataFrame.from_dict(nrmses, orient="index", columns=["Pearson"])
         nrmse_df.plot.bar(ax=summary_ax)
         summary_ax.set_ylim([0, None])
-        summary_ax.set_title(f"RMSE normalizzato tra {ibe_name} e ARPAV")
+        summary_ax.set_title(f"RMSE normalizzato tra {test_name} e {reference_name}")
         for p in summary_ax.patches:
             h = p.get_height()
             label_y = h + 0.05
             summary_ax.annotate(f"{p.get_height():.3f}", (p.get_x() + p.get_width() / 2., label_y), ha="center")
         if save_graphs:
-            graph_directory = Path(f"similarity_graphs/{ibe_name}")
+            graph_directory = Path(f"similarity_graphs/{test_name}")
             graph_directory.mkdir(parents=True, exist_ok=True)
             graph_filename = graph_directory.joinpath(f"nrmse.png")
             plt.savefig(graph_filename)
@@ -219,7 +221,7 @@ def main():
     restricted_arpav_df = arpav_df[(arpav_df.index > f"2020-07-01") & (arpav_df.index < f"2020-07-27")]
     print(restricted_arpav_df)
     # plot_common_variables(restricted_arpav_df, restricted_ibe_df, True)
-    similarity_common_variables(restricted_arpav_df, restricted_ibe_df, sensor_name)
+    similarity_common_variables(restricted_arpav_df, restricted_ibe_df, "ARPAV", sensor_name)
 
 
 if __name__ == '__main__':
