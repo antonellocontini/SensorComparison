6bb34b4f9775242016206ca6f0576e4817360452
Antonello Contini <antonello.contini@outlook.it>
2021-01-11 17:33:19 +0100

    added code documentation

--- lib/arpav.py
+++ lib/arpav.py
@@ -1,7 +1,23 @@
 import pandas as pd
 
 
-def read_ARPAV_station(data_filename: str):
+def read_ARPAV_station(data_filename: str) -> pd.DataFrame:
+    """Legge da .csv i dati della stazione mobile dell'ARPAV
+
+    Parameters
+    ----------
+    data_filename : str
+        Percorso al file .csv da leggere. Il file deve avere il seguente formato:
+
+        Datetime,CO,DVP,NO,NO2,NOx,O3,SO2,T,RH,VVP
+
+        17/06/2020 17,0.116,275,2,4,5,92,3,23,55,0.7
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe con i dati del .csv
+    """
     df = pd.read_csv(data_filename)
     # df["Datetime"] = pd.to_datetime(df["Datetime"], format="%d/%m/%Y %H", utc=True)
 
