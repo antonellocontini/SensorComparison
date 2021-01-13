2eaa642f2fc1fa946515dd9fc8b35d8c5060f9e3
Antonello Contini <antonello.contini@outlook.it>
2021-01-13 16:33:01 +0100

    fix after merge

--- missing_periods.py
+++ missing_periods.py
@@ -1,10 +1,10 @@
-import ibe
+from lib import ibe
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
 def main(sensor_name):
-    ibe_df = ibe.read_IBE_sensor(f"{sensor_name}_extended.json", f"{sensor_name}.params.json")
+    ibe_df = ibe.read_IBE_sensor(f"Dati/Dati IBE/{sensor_name}_extended.json", f"Dati/Dati IBE/{sensor_name}.params.json")
     # ibe_df = ibe_df[ibe_df.index < "2020-08-01"]
     ser = ibe_df.dropna().index.to_series()
     d = ser.diff() > pd.Timedelta(1, "H")
@@ -13,31 +13,32 @@ def main(sensor_name):
     df = pd.DataFrame({"time": d})
     df["shifted"] = df["time"].shift(periods=1)
     df["result"] = 0
-    print(ibe_df.head())
+    # print(ibe_df.head())
     missing_periods = []
     p = []
     for index, row in df.iterrows():
         if row["shifted"] == 0 and row["time"] == 1:
-            print("up", index)
+            # print("up", index)
             df.loc[index, "result"] = 1
             p.append(index)
         elif row["shifted"] == 1 and row["time"] == 0:
             t = index - pd.Timedelta(1, "H")
-            print("down", t)
+            # print("down", t)
             df.loc[t, "result"] = 2
             p.append(t)
             missing_periods.append(p)
             p = []
     missing_df = pd.DataFrame(missing_periods, columns=["from", "to"])
-    missing_df.to_csv(f"{sensor_name}_missing_periods.csv")
-    print(missing_periods)
+    # missing_df.to_csv(f"{sensor_name}_missing_periods.csv")
+    # print(missing_periods)
     # df.plot()
     f, ax = plt.subplots()
     ibe_df.plot(ax=ax, linewidth="0.7")
     for p in missing_periods:
         ax.axvspan(p[0], p[1], color="lightsteelblue")
     ax.legend(prop={'size': 4})
-    plt.savefig(f"{sensor_name}_missing_periods.png", dpi=300)
+    plt.show()
+    # plt.savefig(f"{sensor_name}_missing_periods.png", dpi=300)
 
 
 if __name__ == "__main__":
